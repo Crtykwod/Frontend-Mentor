8d4f9130fa5c286c9e009a3f2e516ad1a2c0677b
Carlos Samuel <crtykwod@gmail.com>
2024-10-09 16:56:49 -0300

    Frontend Mentor - Index

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
+const navButton = document.getElementById("navButton");
 const navList = document.getElementById("navList");
+const langButton = document.getElementById("langButton");
 const langList = document.getElementById("langList");
-const langListPc = document.getElementById("langListPc")
+const langButtonPc = document.getElementById("langButtonPc");
+const langListPc = document.getElementById("langListPc");
 
 function openNav() {
   if (!langList.classList.contains("hidden")) {
-    langList.classList.add("hidden")
+    langList.classList.add("hidden");
   }
+
+  if (navButton.ariaExpanded == "false") {
+    navButton.ariaExpanded = "true";
+  } else {
+    navButton.ariaExpanded = "false";
+  }
+
   navList.classList.toggle("hidden");
 }
 
@@ -13,29 +23,39 @@ function openLang() {
   langList.classList.toggle("hidden");
   langListPc.classList.toggle("hidden");
 
-  if (!navList.classList.contains("hidden")) {
-    navList.classList.add("hidden")
+  if (langButton.ariaExpanded == "false" || langButtonPc.ariaExpanded == "false" ) {
+    langButton.ariaExpanded = "true";
+    langButtonPc.ariaExpanded = "true";
+  } else {
+    langButton.ariaExpanded = "false";
+    langButtonPc.ariaExpanded = "false";
   }
 
-  console.log("pinto")
+  if (!navList.classList.contains("hidden")) {
+    navList.classList.add("hidden");
+  }
 }
 
 function collapseChallenge(button) {
   // Encontra o elemento .challenge__item mais próximo do botão clicado e então encontra o .collapsible dentro do .challenge__item
-  const parentItem = button.closest('.challenge__item');
-  const targetItem = parentItem.querySelector('.collapsible');
+  const parentItem = button.closest(".challenge__item");
+  const targetItem = parentItem.querySelector(".collapsible");
 
   //Retorna erro caso não encontrado
-  if (targetItem == false) {
-    return console.error('Elemento colapsável não encontrado.')
+  if (!targetItem) {
+    return console.error("Elemento colapsável não encontrado.");
   }
 
+  const isVisible = targetItem.style.display === "block";
+
   // Verifica se o conteúdo está visível ou não e alterna
-  if (targetItem.style.display !== "block") {
+  if (!isVisible) {
     targetItem.style.display = "block"; // Mostra o conteúdo
+    button.ariaExpanded = "true";
   } else {
     targetItem.style.display = "none"; // Esconde o conteúdo
+    button.ariaExpanded = "false";
   }
   // Classe para rotacionar o botão
-  button.classList.toggle("active")
+  button.classList.toggle("active");
 }
